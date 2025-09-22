e7ccb0a8c2a4cdaeb79a08522eb33ffd90f569d4
aylaurquizo <urquizo.a@northeastern.edu>
2025-09-22 13:59:31 -0400

    fixing security

--- pantrypal/src/supabaseClient.js
+++ pantrypal/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://bdsifrgbwfdnyuirafdy.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkc2lmcmdid2Zkbnl1aXJhZmR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5MTcwOTAsImV4cCI6MjA3MDQ5MzA5MH0.9umBeOlkt-sG-2b45veUhreLzCknddFp05g2LEfrQIM';
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL
+const SUPABASE_KEY = process.env.REACT_APP_SUPABASE_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
